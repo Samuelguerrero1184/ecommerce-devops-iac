37b59e7c4fc5114e032c1dabd8a57ffec8c7130b
Samuel Guerrero <samuelguerrero@Ordenador-portatil-de-Samuel.local>
2024-04-01 21:59:58 -0500

    First commit

---
+++ main.tf
@@ -0,0 +1,12 @@
+resource "github_branch" "local_1" {
+  repository = "ecommerce-devops-iac"
+  branch     = "local_samuel"
+}
+resource "github_branch" "local_2" {
+  repository = "ecommerce-devops-iac"
+  branch     = "local_santiago"
+}
+resource "github_branch" "local_3" {
+  repository = "ecommerce-devops-iac"
+  branch     = "local_esteban"
+}
